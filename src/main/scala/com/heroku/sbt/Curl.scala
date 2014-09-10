a22a13187af5ab0b2e98490059f9e0405962c475
Joe Kutner <jpkutner@gmail.com>
2014-09-10 14:04:16 -0500

    restructured plugin classes

---
+++ src/main/scala/com/heroku/sbt/Curl.scala
@@ -0,0 +1,29 @@
+package com.heroku.sbt
+
+import java.io.{InputStreamReader, BufferedReader}
+import java.net.URL
+import javax.net.ssl.HttpsURLConnection
+
+object Curl {
+  def apply(urlStr: String, method: String, data: String, headers: Map[String, String]): String = {
+    val url = new URL(urlStr)
+    val con = url.openConnection.asInstanceOf[HttpsURLConnection]
+    con.setDoInput(true)
+    con.setDoOutput(true)
+    con.setRequestMethod(method)
+
+    headers.foreach { case (key, value) => con.setRequestProperty(key, value)}
+
+    con.getOutputStream.write(data.getBytes("UTF-8"))
+
+    val reader = new BufferedReader(new InputStreamReader(con.getInputStream))
+
+    var output = ""
+    var tmp = reader.readLine
+    while (tmp != null) {
+      output += tmp
+      tmp = reader.readLine
+    }
+    output
+  }
+}
