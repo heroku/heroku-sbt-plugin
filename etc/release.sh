4e2fa774303c65d50158b2b81ae3fc4f399d5602
Joe Kutner <jpkutner@gmail.com>
2014-10-28 10:44:25 -0500

    added release scripts

---
+++ etc/release.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# fail hard
+set -o pipefail
+# fail harder
+set -eu
+
+. ./etc/semver.sh
+
+CUR_VERSION="$(grep "git.baseVersion :=" "version.sbt" | sed -E -e 's/git.baseVersion := //g' | sed 's/"//g')"
+
+#sbt "release v${CUR_VERSION}"
+
+MAJOR=0
+MINOR=0
+PATCH=0
+SPECIAL=""
+
+semverParseInto "${CUR_VERSION}" MAJOR MINOR PATCH SPECIAL
+
+NEW_VERSION="${MAJOR}.${MINOR}.$(($PATCH + 1))"
+
+sed -e s/${CUR_VERSION}/${NEW_VERSION}/g version.sbt > version.sbt.tmp
+mv version.sbt.tmp version.sbt
