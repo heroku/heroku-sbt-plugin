932547c06398597d178a7fd0cd7937f44c942e26
Joe Kutner <jpkutner@gmail.com>
2014-11-19 13:50:16 -0600

    upgrade to heroku-deploy 0.1.5

--- src/main/scala/com/heroku/sbt/SbtApp.scala
+++ src/main/scala/com/heroku/sbt/SbtApp.scala
@@ -1,12 +1,10 @@
 package com.heroku.sbt
 
-import sbt._
 import java.io.File
 import java.net.URL
-import java.nio.charset.StandardCharsets
-import java.nio.file.{Paths, Files}
-import com.heroku.sdk.deploy.{Curl, Slug, App}
-import sbt.Logger
+
+import com.heroku.sdk.deploy.{App, Curl}
+import sbt.{Logger, _}
 import sbt.compiler.CompileFailed
 
 import scala.collection.JavaConversions
@@ -70,7 +68,7 @@ class SbtApp(buildPackDesc:String, name:String, rootDir:File, targetDir:File, lo
     }
   }
 
-  override def prepare(includedFiles:java.util.List[java.io.File], jdkVersion:String, jdkUrl:URL): Unit = {
+  override def prepare(includedFiles:java.util.List[java.io.File]): Unit = {
     val defaultIncludedFiles = packageType match {
       case Universal(dir) =>
         Seq(dir)
@@ -81,9 +79,9 @@ class SbtApp(buildPackDesc:String, name:String, rootDir:File, targetDir:File, lo
     // OMG
     val javaIncludedFiles = JavaConversions.seqAsJavaList(defaultIncludedFiles ++ JavaConversions.collectionAsScalaIterable(includedFiles))
 
-    super.prepare(javaIncludedFiles, jdkVersion, jdkUrl)
+    super.prepare(javaIncludedFiles)
 
-    addSlugExtras(jdkVersion, jdkUrl)
+    sbt.IO.copyDirectory(targetDir / "resolution-cache" / "reports", getAppDir / "target" / "resolution-cache" / "reports")
   }
 
   def packageType: PackageType = {
@@ -94,25 +92,6 @@ class SbtApp(buildPackDesc:String, name:String, rootDir:File, targetDir:File, lo
     } else {
       throw new CompileFailed(Array(), "You must stage your application before deploying it!", Array())
     }
-  }
-
-  def addSlugExtras(jdkVersion:String, jdkUrl: URL) {
-    sbt.IO.copyDirectory(targetDir / "resolution-cache" / "reports", getAppDir / "target" / "resolution-cache" / "reports")
-    val v:String = if (Option[String](jdkVersion).isDefined) {
-      jdkVersion
-    } else if (jdkUrl.toString.contains("openjdk1.8-")) {
-      "1.8"
-    } else if (jdkUrl.toString.contains("openjdk1.7-")) {
-      "1.7"
-    } else if (jdkUrl.toString.contains("openjdk1.6-")) {
-      "1.6"
-    } else {
-      return
-    }
-    Files.write(Paths.get((getAppDir / "system.properties").getPath),
-      s"""
-      |java.runtime.version=$v
-      """.stripMargin('|').getBytes(StandardCharsets.UTF_8))
   }
 
 }
