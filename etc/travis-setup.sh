0c25bd7bd83e28acb9eaeabae3b8f0c01960d217
Joe Kutner <jpkutner@gmail.com>
2017-06-09 13:01:44 -0500

    Updated travis setup

--- etc/travis-setup.sh
+++ etc/travis-setup.sh
@@ -1,17 +1,23 @@
-if [ -n "`git config --get user.email`" ]; then
-  echo 'already set'; else `git config --global user.email 'buildpack@example.com'`
-fi
-if [ -n "`git config --get user.name`" ]; then
-  echo 'already set'; else `git config --global user.name 'BuildpackTester'`
-fi
+#!/usr/bin/env bash
 
-cat <<EOF > ~/.ssh/config
+[ "$TRAVIS" != "true" ] && echo "Not running on Travis!" && exit 1
+
+git config --global user.email ${HEROKU_API_USER:-"buildpack@example.com"}
+git config --global user.name 'BuildpackTester'
+
+cat <<EOF >> ~/.ssh/config
 Host heroku.com
     StrictHostKeyChecking no
     CheckHostIP no
     UserKnownHostsFile=/dev/null
 Host github.com
     StrictHostKeyChecking no
+EOF
+
+cat <<EOF >> ~/.netrc
+machine git.heroku.com
+  login ${HEROKU_API_USER:-"buildpack@example.com"}
+  password ${HEROKU_API_KEY:-"password"}
 EOF
 
 curl --fail --retry 3 --retry-delay 1 --connect-timeout 3 --max-time 30 https://toolbelt.heroku.com/install-ubuntu.sh | sh
