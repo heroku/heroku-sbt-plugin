f5e55e53ac69e16a4bd59258de12b60bf21c72fe
Manuel Fuchs <malax@malax.de>
2020-01-21 09:46:04 +0100

    Remove obsolete release tooling

--- release.sh
+++ release.sh
@@ -8,3 +8,13 @@ set -eu
 sbt "^^ 0.13.18" publishLocal
 sbt "^^ 1.0.0" publishLocal
 sbt release
+
+echo "Now make sure you update these articles and projects:
+
+    https://devcenter.heroku.com/articles/deploying-scala-and-play-applications-with-the-heroku-sbt-plugin
+    https://devcenter.heroku.com/articles/deploy-scala-and-play-applications-to-heroku-from-jenkins-ci
+    https://devcenter.heroku.com/articles/deploy-scala-and-play-applications-to-heroku-from-travis-ci
+    https://github.com/jkutner/heroku-jenkins-scala-example
+    https://github.com/jkutner/travis-heroku-scala-example
+    https://github.com/kissaten/sbt-heroku-play-example
+"
